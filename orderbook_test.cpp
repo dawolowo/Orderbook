242bb89e24ac5b148a50da849e2261d400475aca
David <113637215+dawolowo@users.noreply.github.com>
2024-10-29 20:21:57 +0100

    Update orderbook_test.cpp

--- orderbook_test.cpp
+++ orderbook_test.cpp
@@ -1,31 +1,31 @@
-#include "orderbook.h"
+#include "orderbook.hpp"
 using namespace std;
 
 int main(){
-    OrderBook btc_OB;
+    OrderBook btc_OB(60200);
 
-    Price prices[] = {60000, 60100, 60200, 60400, 60400, 60300, 60800, 60152, 60355, 60452};
+    Price prices[] = {60000, 60300, 60050, 60400, 60400, 60054, 60800, 61502, 60355, 60195};
 
     OrderDirection directions[] = {
         OrderDirection::buy,OrderDirection::sell, OrderDirection::buy,OrderDirection::sell,OrderDirection::sell, OrderDirection::buy, OrderDirection::sell,
         OrderDirection::sell,OrderDirection::sell,OrderDirection::buy,};
 
-    OrderId ids[] = {get_order_id(),get_order_id(),get_order_id(),get_order_id(),get_order_id(),
-        get_order_id(),get_order_id(),get_order_id(),get_order_id(),get_order_id()};
-
     Quantity quantites[] = {10,89,11,45,66,1,2,4,26,61};
-
+    OrderId ids[10];
     for (int i = 0; i < 10; i++){
-        btc_OB.add_order(Order(prices[i], quantites[i], OrderType::gtc, directions[i], ids[i]));
+        Order order = Order(prices[i], quantites[i], OrderType::limit, directions[i]);
+        ids[i] = order.order_id();
+        btc_OB.add_order(order);
     }
+    // btc_OB.cancel_order(ids[0]);
+    btc_OB.add_order(Order(61055, 226, OrderType::mo, OrderDirection::sell));
+    btc_OB.match();
 
-    cout << btc_OB << endl;
-
-    LevelsInfo asks = btc_OB.get_asks();
-
-    for (auto &ask : asks){
-        cout << ask << endl;
-    }
-
+    cout << btc_OB << "\n";
+    cout << btc_OB.last_price() << "\n";
+    for (const auto & x : btc_OB.time_sales()){
+        cout << x << "\n";
+    }   
+    
     return 0;
 }
