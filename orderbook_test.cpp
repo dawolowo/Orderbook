852241d20f337ac976337789f15fe52724bf69c3
David <113637215+dawolowo@users.noreply.github.com>
2024-10-30 06:58:29 +0100

    Update orderbook_test.cpp

--- orderbook_test.cpp
+++ orderbook_test.cpp
@@ -13,12 +13,15 @@ int main(){
     Quantity quantites[] = {10,89,11,45,66,1,2,4,26,61};
     OrderId ids[10];
     for (int i = 0; i < 10; i++){
-        Order order = Order(prices[i], quantites[i], OrderType::limit, directions[i]);
+        Order order(prices[i], quantites[i], OrderType::limit, directions[i]);
         ids[i] = order.order_id();
         btc_OB.add_order(order);
     }
     // btc_OB.cancel_order(ids[0]);
-    btc_OB.add_order(Order(61055, 226, OrderType::mo, OrderDirection::sell));
+    Order stop(60250, 3, OrderType::stop, OrderDirection::buy);
+    btc_OB.add_order(Order(61055, 61, OrderType::mo, OrderDirection::buy));
+    btc_OB.add_order(stop);
+    btc_OB.cancel_order(stop.order_id());
     btc_OB.match();
 
     cout << btc_OB << "\n";
